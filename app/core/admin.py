c508e585b1e215f5cf6c4cb90e964faf6e6e331e
Лычагин Сергей <serg.programmer777@gmail.com>
2022-10-27 16:25:40 +0300

    Build Recipe API

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
-from .models import User
+from .models import User, Recipe
 
 
 class UserAdmin(BaseUserAdmin):
@@ -40,3 +40,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Recipe)
