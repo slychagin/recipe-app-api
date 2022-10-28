c5d547a4af23792d24da4d12f1cb82aaea831890
Лычагин Сергей <serg.programmer777@gmail.com>
2022-10-28 17:04:30 +0300

    Build tags API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,6 +5,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Create and return a new user"""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """Tests models"""
 
@@ -62,3 +67,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creatin a tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
